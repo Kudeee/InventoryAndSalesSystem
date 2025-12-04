7ad472b21a4f7d1990378a62e49ed5911e0d5d00
kudeee <codysu08@gmail.com>
2025-12-04 17:30:17 +0800

    fix the sales bug

--- InventoryAndSalesSystem/Helpers/ValueConverters.cs
+++ InventoryAndSalesSystem/Helpers/ValueConverters.cs
@@ -56,4 +56,21 @@ namespace InventoryAndSalesSystem.Helpers
             throw new NotImplementedException();
         }
     }
+
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double productPrice = System.Convert.ToDouble(values[0]);
+            double sellQuantity = System.Convert.ToDouble(values[1]);
+            
+            return productPrice * sellQuantity;
+            
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
